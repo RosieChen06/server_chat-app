435d21abf78540e4e6884108c6cc4c35345c0ee6
RosieChen06 <katekyo.069@gmail.com>
2025-03-14 12:10:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -291,9 +291,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRouter)
 
-server.listen(3001, () => {
-    console.log('server is running on port: 3001')
-})
+server.listen(process.env.PORT || 3001, () => {
+    console.log(`Server is running on port: ${process.env.PORT || 3001}`);
+});
 
 
 
